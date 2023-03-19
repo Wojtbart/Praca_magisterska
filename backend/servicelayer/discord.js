d5df40b9be8071faec6a4c52a264b58805e3e8de
Wojtbart <wsekowski@poczta.onet.pl>
2023-03-19 21:34:10 +0100

    poprawki

--- backend/servicelayer/discord.js
+++ backend/servicelayer/discord.js
@@ -3,8 +3,9 @@ const config = ini.parse(fs.readFileSync('../../config.ini','utf-8'));
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-const friendsChannelId = '1084577436936704130';
-const myUserId = '632611376153755665';
+const accountSid = config.tokens.TWILIO_ACCOUNT_SID;
+const authToken = config.tokens.TWILIO_AUTH_TOKEN;
+const twilio = require('twilio')(accountSid, authToken);
 
 function sendSMS(sms) {
   twilio.messages.create({
@@ -17,27 +18,27 @@ function sendSMS(sms) {
 
 client.on("messageCreate", function(message) {
   if (message.author.bot) return;
-   if (message.channel.id == friendsChannelId && message.author.id != myUserId) {
+   if (message.channel.id == config.discord.friendsChannelId && message.author.id != config.discord.myUserId) {
        const sms = `${message.author.username}: ${message.content}`;
        sendSMS(sms);
    }
-   else if(message.mentions.members.has(myUserId)) {
+   else if(message.mentions.members.has(config.discord.myUserId)) {
        const sms = `${message.author.username} mentioned you in ${message.channel.name}: ${message.content}`;
        sendSMS(sms);
    }
 });
-client.on("guildMemberAdd", function(member) {
-  sendSMS(`${member.user.username} has joined the server!`)
-});
+// client.on("guildMemberAdd", function(member) {
+//   sendSMS(`${member.user.username} has joined the server!`)
+// });
 
-client.on("guildMemberRemove", function(member) {
-  sendSMS(`${member.user.username} has left the server!`)
-});
+// client.on("guildMemberRemove", function(member) {
+//   sendSMS(`${member.user.username} has left the server!`)
+// });
 
 
 app.post('/sms', async (req, res) => {
   if(req.body.From != config.tokens.PERSONAL_NUMBER) return;
-  const channel = client.channels.cache.get(friendsChannelId);
+  const channel = client.channels.cache.get(config.discord.friendsChannelId);
   channel.send(req.body.Body);
 
   res.status(201).send({
@@ -57,4 +58,26 @@ app.post('/sms', async (req, res) => {
 // });
 
 // client.login(TOKEN);
+
+const userDatabase = [];
+
+app.post('/testowa', (req, res) => {
+  const { email, password } = req.body;
+  const user = {
+    email,
+    password
+  };
+
+  userDatabase.push(user);
+
+  const welcomeMessage = 'Wiadomośc testowa Wojciecha';
+
+  sendSMS(welcomeMessage);
+
+  res.status(201).send({
+    message: 'Account created successfully, kindly check your phone to activate your account!',
+    data: user
+  })
+  
+});
 client.login(config.tokens.BOT_TOKEN);
